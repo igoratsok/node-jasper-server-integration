980daf456176f59175401defdb4833566b3c3ef6
Igor Augusto <igorgutofc@gmail.com>
2020-05-18 18:19:26 -0300

    First commit. Adding the JSIntegration module.

---
+++ src/JSIntegration.js
@@ -0,0 +1,71 @@
+module.exports = class JSIntegration {
+    constructor(jasperUrl, reportPath, type, user, password, parameters) {
+        this.jasperUrl = jasperUrl;
+        this.reportPath = reportPath;
+        this.type = type;
+        this.user = user;
+        this.password = password;
+
+        if (typeof parameters === 'undefined') {
+            this.parameters = [];
+        } else {
+            this.parameters = parameters;
+        }
+        
+
+        if (this.jasperUrl.indexOf('/') === this.jasperUrl.length-1) {
+            this.jasperUrl = this.jasperUrl.substr(1, this.jasperUrl.length - 2);
+        }
+
+        if (this.reportPath.indexOf('/') === this.reportPath.length-1) {
+            this.reportPath = this.reportPath.substr(1, this.reportPath.length - 2);
+        }
+    }
+
+    addParameter(parameter, value) {
+        this.parameters.push({parameter : parameter, value : value});
+    }
+
+    _getQueryString() {
+        var queryString = '';
+
+        for(var i = 0; i < this.parameters.length; i++) {
+            queryString += (queryString === '' ? '?' : '&') + this.parameters[i].parameter + '=' + this.parameters[i].value;
+        }
+
+        return queryString;
+    }
+
+    execute() {
+        return new Promise((resolve, reject) => {
+            var http = require('http');
+            var url = '';
+
+            var options = {
+                auth : this.user + ':' + this.password
+            };
+
+            url += this.jasperUrl + '/rest_v2/reports/' + this.reportPath + '.' + this.type + this._getQueryString();
+
+            http.get(
+                url,
+                options,
+                (res) => {
+                    let rawData = '';
+                    res.on('data', (chunk) => {
+                        rawData += chunk;
+                    });
+
+                    res.on('end', () => {
+                        resolve(rawData);
+                    });
+
+                    res.on('error', (e) => {
+                        reject(e.message);
+                    });
+                }
+
+            )
+        });
+    }
+}
